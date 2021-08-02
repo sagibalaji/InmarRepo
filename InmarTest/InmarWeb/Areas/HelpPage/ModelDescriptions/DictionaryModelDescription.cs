dbac8779922e4e5ff25b8c6e2144da58bcbcfea8
sagibalaji <sagibalaji0450@gmail.com>
2021-08-02 12:21:57 -0600

    Delete InmarTest/InmarWeb directory

--- InmarTest/InmarWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace InmarWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- InmarTest/InmarWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace InmarWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
